64c21687b9dfe4d3afc0c7add22398152e512426
Olaf <67223543+olaf-wilkosz@users.noreply.github.com>
2022-08-31 00:40:18 +0200

    Replace audioUrl with currentSong object

--- src/App.js
+++ src/App.js
@@ -2,11 +2,16 @@ import './styles.css';
 import SongPlayer from './SongPlayer';
 
 function App() {
-	const audioUrl = 'https://examples.devmastery.pl/assets/audio/deadfro5h.mp3';
+	const currentSong = {
+		audioUrl: 'https://examples.devmastery.pl/assets/audio/deadfro5h.mp3',
+		coverUrl: 'https://examples.devmastery.pl/assets/audio/deadfro5h.jpg',
+		title: 'Deadfro5h',
+		artist: 'starfrosh',
+	};
 
 	return (
 		<div className="App">
-			<SongPlayer audioUrl={audioUrl} />
+			<SongPlayer song={currentSong} />
 		</div>
 	);
 }
--- src/SongPlayer.js
+++ src/SongPlayer.js
@@ -2,8 +2,9 @@ function SongPlayer(props) {
 	return (
 		<section>
 			<h1>React Music Player</h1>
+			<img src={props.song.coverUrl} alt="Song cover" width="250" height="250" />
 			<audio controls>
-				<source src={props.audioUrl} />
+				<source src={props.song.audioUrl} />
 			</audio>
 		</section>
 	);
