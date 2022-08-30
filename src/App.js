89910deb44e8e74c1ba461d3725a5e8ecbae103b
Olaf <67223543+olaf-wilkosz@users.noreply.github.com>
2022-08-31 00:42:40 +0200

    Add additional song objects

--- src/App.js
+++ src/App.js
@@ -2,12 +2,28 @@ import './styles.css';
 import SongPlayer from './SongPlayer';
 
 function App() {
-	const currentSong = {
-		audioUrl: 'https://examples.devmastery.pl/assets/audio/deadfro5h.mp3',
-		coverUrl: 'https://examples.devmastery.pl/assets/audio/deadfro5h.jpg',
-		title: 'Deadfro5h',
-		artist: 'starfrosh',
-	};
+	const songs = [
+		{
+			audioUrl: 'https://examples.devmastery.pl/assets/audio/deadfro5h.mp3',
+			coverUrl: 'https://examples.devmastery.pl/assets/audio/deadfro5h.jpg',
+			title: 'Deadfro5h',
+			artist: 'starfrosh',
+		},
+		{
+			audioUrl: 'https://examples.devmastery.pl/assets/audio/majesty.mp3',
+			coverUrl: 'https://examples.devmastery.pl/assets/audio/majesty.jpg',
+			title: 'Majesty (Original Mix)',
+			artist: 'Ryan Craig Martin',
+		},
+		{
+			audioUrl: 'https://examples.devmastery.pl/assets/audio/runs.mp3',
+			coverUrl: 'https://examples.devmastery.pl/assets/audio/runs.jpg',
+			title: 'Runs',
+			artist: 'Wowa',
+		},
+	];
+
+	const currentSong = songs[2];
 
 	return (
 		<div className="App">
