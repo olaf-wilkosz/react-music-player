08408a9604e1bd141d4ca01463704d1936c7da5c
Olaf <67223543+olaf-wilkosz@users.noreply.github.com>
2022-08-31 00:26:26 +0200

    Add audio element

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './styles.css';
 
 function App() {
+	const audioUrl = 'https://examples.devmastery.pl/assets/audio/deadfro5h.mp3';
+
 	return (
 		<div className="App">
 			<h1>React Music Player</h1>
+			<audio controls>
+				<source src={audioUrl} />
+			</audio>
 		</div>
 	);
 }
