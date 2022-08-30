ce91c230f1e8945953f102a4ea7b2aefe48ae704
Olaf <67223543+olaf-wilkosz@users.noreply.github.com>
2022-08-31 00:44:19 +0200

    Add handling player reload on song change

--- src/SongPlayer.js
+++ src/SongPlayer.js
@@ -3,7 +3,7 @@ function SongPlayer(props) {
 		<section>
 			<h1>React Music Player</h1>
 			<img src={props.song.coverUrl} alt="Song cover" width="250" height="250" />
-			<audio controls>
+			<audio key={props.song.audioUrl} controls>
 				<source src={props.song.audioUrl} />
 			</audio>
 		</section>
