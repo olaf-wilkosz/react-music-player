22b67d61750109cdf2f9fba1bf8d6147693287d3
Olaf <67223543+olaf-wilkosz@users.noreply.github.com>
2022-08-31 00:12:25 +0200

    Add basic styles

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import './styles.css';
 
 function App() {
-	return <div className="App"></div>;
+	return (
+		<div className="App">
+			<h1>React Music Player</h1>
+		</div>
+	);
 }
 
 export default App;
