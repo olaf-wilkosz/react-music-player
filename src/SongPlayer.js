661d2b44c9b2bdaeec87fcb690036c4aa79d0211
Olaf <67223543+olaf-wilkosz@users.noreply.github.com>
2022-08-31 02:31:43 +0200

    Add custom playback controls

--- src/SongPlayer.js
+++ src/SongPlayer.js
@@ -1,9 +1,17 @@
+import { useRef } from 'react';
+
 function SongPlayer(props) {
+	const audioRef = useRef();
+
 	return (
 		<section>
 			<h1>React Music Player</h1>
 			<img src={props.song.coverUrl} alt="Song cover" width="250" height="250" />
-			<audio key={props.song.audioUrl} controls>
+			<div>
+				<button onClick={() => audioRef.current.play()}>Play</button>
+				<button onClick={() => audioRef.current.pause()}>Pause</button>
+			</div>
+			<audio ref={audioRef} key={props.song.audioUrl}>
 				<source src={props.song.audioUrl} />
 			</audio>
 		</section>
