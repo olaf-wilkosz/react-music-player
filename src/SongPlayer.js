9b0e7bd48c2774f4d9c01c57887794f2caf3509d
Olaf <67223543+olaf-wilkosz@users.noreply.github.com>
2022-09-03 22:19:54 +0200

    Add stopping the playback

--- src/SongPlayer.js
+++ src/SongPlayer.js
@@ -1,9 +1,18 @@
 import { useRef } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBackward, faPlay, faPause, faStop, faForward } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBackward, faPlay, faPause, faStop, faForward } from '@fortawesome/free-solid-svg-icons';
 
 function SongPlayer(props) {
 	const audioRef = useRef();
+
+	Audio.prototype.stop = function () {
+		this.pause();
+		this.currentTime = 0;
+	};
+
+	const playSong = () => audioRef.current.play();
+	const pauseSong = () => audioRef.current.pause();
+	const stopSong = () => audioRef.current.stop();
 
 	return (
 		<section className="SongPlayerWrapper">
@@ -15,11 +24,21 @@ function SongPlayer(props) {
 				<h1>React Music Player</h1>
 				<img src={props.song.coverUrl} alt="Song cover" />
 				<div>
-					<button id='prevBtn' disabled><FontAwesomeIcon icon={faBackward} /></button>
-					<button onClick={() => {audioRef.current.play()}} id='playBtn'><FontAwesomeIcon icon={faPlay} /></button>
-					<button onClick={() => {audioRef.current.pause()}} id='pauseBtn'><FontAwesomeIcon icon={faPause} /></button>
-					<button id='stopBtn' disabled><FontAwesomeIcon icon={faStop} /></button>
-					<button id='nextBtn' disabled><FontAwesomeIcon icon={faForward} /></button>
+					<button id="prevBtn" disabled>
+						<FontAwesomeIcon icon={faBackward} />
+					</button>
+					<button onClick={() => playSong()} id="playBtn">
+						<FontAwesomeIcon icon={faPlay} />
+					</button>
+					<button onClick={() => pauseSong()} id="pauseBtn">
+						<FontAwesomeIcon icon={faPause} />
+					</button>
+					<button onClick={() => stopSong()} id="stopBtn">
+						<FontAwesomeIcon icon={faStop} />
+					</button>
+					<button id="nextBtn" disabled>
+						<FontAwesomeIcon icon={faForward} />
+					</button>
 				</div>
 				<audio ref={audioRef} key={props.song.audioUrl}>
 					<source src={props.song.audioUrl} />
